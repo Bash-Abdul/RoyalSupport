8f1b78fe24d86e14553543eae2b6e6caa1425d32
Bash-Abdul <bashirtaiwo59@gmail.com>
2023-11-28 14:06:58 +0100

    made updates

---
+++ JS/scroll.js
@@ -0,0 +1,17 @@
+ScrollReveal({
+    // reset: true,
+    distance: '200px',
+    duration: 3000,
+    delay: 200
+});
+
+
+
+ScrollReveal().reveal('.main', { origin: 'top' });
+
+
+ScrollReveal().reveal('.main_content, .one, .inputs', { origin: 'left' });
+
+ScrollReveal().reveal('.two,.three,.home_btn', { origin: 'right' });
+
+ScrollReveal().reveal('')
